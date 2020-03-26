cd6aa7352e3768ac9a22c35c128e017e1e9d6afc
RobertasTutlys <robertastut@gmail.com>
2020-03-26 12:37:06 +0100

    Updated: map impl to manage setting of ID Property

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -22,37 +22,33 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Michael");
         owner1.setLastName("Weston");
-
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Fiona");
         owner2.setLastName("Glenanne");
-
         ownerService.save(owner2);
 
         System.out.println("Loaded owners ....");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Sam");
         vet1.setLastName("Exe");
-
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
-
         vetService.save(vet2);
 
-        System.out.println("Loaded vets ....");
+        Vet vet3 = new Vet();
+        vet3.setFirstName("Roger");
+        vet3.setLastName("Smith");
+        vetService.save(vet3);
 
+        System.out.println("Loaded vets ....");
 
     }
 }
