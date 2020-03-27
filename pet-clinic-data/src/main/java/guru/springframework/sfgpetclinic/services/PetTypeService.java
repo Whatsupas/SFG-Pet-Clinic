8c0096d5a332bfff3b76b7b47c23ea7ccb76e91f
RobertasTutlys <robertastut@gmail.com>
2020-03-27 13:11:48 +0100

    Created PetType Map Service. Closes #29.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created by Robertas Tutlys on 2020-03-27
+ */
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
