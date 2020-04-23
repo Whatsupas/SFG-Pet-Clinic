01b41df7f72ed1ff372194fc2ecfbf66308e290a
RobertasTutlys <robertastut@gmail.com>
2020-04-23 21:56:39 +0200

    Converted Vet to JPA entity. Closes #37

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -1,9 +1,17 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by Robertas Tutlys on 2020-03-27
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
+
+    @Column(name ="description")
     private String description;
 
     public String getDescription() {
