c735c7c9bcf7ef8457ae6f1e08076dbe5eb58634
RobertasTutlys <robertastut@gmail.com>
2020-04-24 09:36:44 +0200

    Created Spring Data JPA Service for Owners. Closes #40

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/repositories/OwnerRepository.java
@@ -3,9 +3,13 @@ package guru.springframework.sfgpetclinic.repositories;
 import guru.springframework.sfgpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 /**
  * Created by Robertas Tutlys on 2020-04-24
  */
 public interface OwnerRepository extends CrudRepository<Owner,Long> {
 
+    Owner findByLastName(String lastName);
+    Set<Owner> findAll();
 }
